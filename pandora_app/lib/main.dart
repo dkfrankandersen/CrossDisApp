36661ebf4de285e126e52a72b3b891c7382d8a00
fand <fand@itu.dk>
2020-11-06 09:24:08 +0100

    just a commit

--- pandora_app/lib/main.dart
+++ pandora_app/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
